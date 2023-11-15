d184015f65bb877e54578d11097d93cea0274466
Monzer7 <monzer-7@hotmail.com>
2023-11-15 12:52:15 -0800

    adding main file

---
+++ get_path.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * get_location -  to cat the path
+ * @command: to find path to
+ * Return: addres to command
+ */
+
+char *get_location(char *command)
+{
+	char *location, *location_cp, *location_token, *location_path;
+	int lenght, dr_lenght;
+	struct stat buf;
+
+	location = getenv("PATH");
+	if (location)
+	{
+		lenght = strlen(command);
+		location_cp = strdup(location);
+		location_token = strtok(location_cp, ":");
+	while (location_cp != NULL)
+	{
+		dr_lenght = strlen(location_token);
+		location_path = malloc(lenght + dr_lenght + 2);
+		strcpy(location_path, location_token);
+		strcat(location_path, "/");
+		strcat(location_path, command);
+		strcat(location_path, "\0");
+
+	if (stat(location_path, &buf) == 0)
+	{
+		free(location_cp);
+		return (location_path);
+	}
+		else
+		{
+			free(location_path);
+			location_token = strtok(NULL, ":");
+		}
+	} /*loop end*/
+		free(location_cp);
+		if (stat(command, &buf) == 0)
+	{
+		return (command);
+	}
+		return (command);
+	}
--- main.c
+++ main.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <sys/types.h>
-#include <unistd.h>
-
-void task1(char *argv[], char **env);
 /**
  * main - main function.
  * @ac: number of arguments
@@ -10,80 +6,11 @@ void task1(char *argv[], char **env);
  * @env: environment
  * Return: number of arguments
 */
+
 int main(int ac, char **argv, char **env)
 {
-	if (ac == 1)
-		task1(argv, env);
+	if (ac <= 12) /* changed from 1 to 12*/
+	shell(argv, env);
+
 	return (0);
-
-}
-/**
- * task1 - creat a simple shell.
- * @argv: arguments
- * @env: environment
- * Return: the command line arguments
-*/
-void task1(char *argv[], char **env)
-{
-/*setting variable*/
-	pid_t proc_id;
-	char *prompt = "$ ";
-	char *command = NULL, *string;
-	size_t n = 0;
-	ssize_t char_num;
-	int i, status;
-
-	argv[0] = NULL;
-	argv[1] = NULL;
-
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-		printf("%s", prompt);/*printing prompt*/
-		char_num = getline(&command, &n, stdin);/*getting command*/
-
-		if (char_num == -1)
-		{
-			free(command);
-			exit(EXIT_FAILURE);
-		}
-
-		/*allocate space for string*/
-		string = malloc(sizeof(char) * (char_num - 1));
-		if (string == NULL)
-			exit(EXIT_FAILURE);
-
-		/*deleting the NULL operator*/
-		i = 0;
-		while (command[i] != '\0')
-		{
-			string[i - 1] = command[i - 1];
-			i++;
-		}
-		argv[0] = string;
-		argv[1] = NULL;
-
-		/*creating a child proceess to make*/
-
-		proc_id = fork();
-		if (proc_id == -1)
-		{
-			free(string);
-			free(command);
-			exit(EXIT_FAILURE);
-		}
-		if (proc_id == 0)
-		{
-			execve(argv[0], argv, env);
-			printf("no such file or directory\n");
-		}
-		else
-			wait(&status);
-	}
-	/* Handle error as needed */
-
-	/*freeing*/
-	free(string);
-	free(argv);
-	free(command);
 }
